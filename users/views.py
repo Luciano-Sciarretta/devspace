3488fb8de14d68b2b640cfbeaf3a434efd2f8f21
Luciano Sciarretta <clsciarretta@hotmail.com>
2024-10-11 16:18:25 -0300

    Antes de migrar Skill

--- users/views.py
+++ users/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Profile
 
 def profiles(request):
-    return render(request, "users/profiles.html")
+    profiles = Profile.objects.all()
+    context = {
+        "profiles": profiles
+    }
+    return render(request, "users/profiles.html", context)
+
 
 def profile(request, pk):
     return render(request, "users/profile.html")
