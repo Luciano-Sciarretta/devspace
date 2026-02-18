a1e2e737e02c92195517f98c5fc4449d7693eb4d
Luciano Sciarretta <clsciarretta@hotmail.com>
2026-02-18 16:33:05 -0300

    Eliminando rastro de browser_reload en urls.py

--- devspace/urls.py
+++ devspace/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("__reload__/", include("django_browser_reload.urls")),
     path('admin/', admin.site.urls, name='admin:index'),
     path("projects/", include("projects.urls")),
     path("", include("users.urls")),
