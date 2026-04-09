d30f014908b3b786139c48903eb37287bc97497c
Luciano Sciarretta <clsciarretta@hotmail.com>
2026-04-09 10:26:43 -0300

    Correción de whitenise storage

--- devspace/settings.py
+++ devspace/settings.py
@@ -190,7 +190,7 @@ if USE_CLOUDINARY:
             "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
         },
         "staticfiles": {
-            "BACKEND": "whitenoise.storage.StaticFilesStorage",
+            "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
         },
     }
       MEDIA_URL = '/media/'
